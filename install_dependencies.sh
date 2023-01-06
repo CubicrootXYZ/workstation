a56111ba0bbcf41aed8959269a9acb4d7fe27110
cubicroot2 <github@cubicroot.xyz>
2023-01-06 19:49:12 +0100

    fixi nstall

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,2 +1,5 @@
-sudo apt install ansible
+apt install python3
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+python3 -m pip install ansible
 ansible-galaxy install petermosmans.customize-gnome
