8f96a9a1b936dc1e506357803a72b40090c6ecbf
cubicroot <github@cubicroot.xyz>
2025-12-07 15:32:40 +0100

    fix: use fork of petermosmans.customize-gnome that is compatible with ansible >= 12

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,5 +2,4 @@ apt update && apt upgrade -y
 apt install python3 curl python3-pip -y
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install ansible ansible-core -y
-ansible-galaxy install -p ./roles petermosmans.customize-gnome
-ansible-galaxy install -r roles/petermosmans.customize-gnome/requirements.yml
+ansible-galaxy install -r requirements.yml
